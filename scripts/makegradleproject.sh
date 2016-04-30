b16f8cc34a98e1bb05f7ed9be17e80a46ffaff70
Simao <xdevs23@users.noreply.github.com>
2016-04-30 16:03:29 +0200

    scripts: add makegradleproject.sh
    This script allows you to import gradle projects.
    CD to the target package (e.g. packages/apps/Settings) and execute the script.
    Make sure to specify the correct path to the template inside the script.

---
+++ scripts/makegradleproject.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+TEMPLATE_PATH="/home/simao/xos/srcs/AOSPTemplate"
+CURRENT_DIR_NAME="${PWD##*/}"
+APP_PATH="$(pwd)"
+EDIT_SCRIPT_FILES="false"
+
+if [ "$1" == "--undo" ]; then
+  cd $TEMPLATE_PATH
+  ALLFILES=$(find .)
+  cd $APP_PATH
+  rm -rf app
+  for word in $ALLFILES; do
+    if [ ! $word == "." ]; then
+      rm -rf $word
+    fi
+  done
+  EDIT_SCRIPT_FILES="false"
+elif [ "$1" == "--cp-scripts" ]; then
+  cp $TEMPLATE_PATH/configure.sh ./
+  cp $TEMPLATE_PATH/debugapp.sh ./
+  cp $TEMPLATE_PATH/buildDebugApp.sh ./
+  EDIT_SCRIPT_FILES="true"
+elif [ "$1" == "--add-gitignore" ]; then
+  cp $TEMPLATE_PATH/.gitignore ./
+  EDIT_SCRIPT_FILES="false"
+else
+  RESULT_OF_CP=0
+  if [ -e "src/" ]; then
+    cp -R $TEMPLATE_PATH/* ./
+    cp $TEMPLATE_PATH/.gitignore ./
+    ln -s $APP_PATH/src app/src/main/java
+    ln -s $APP_PATH/res app/src/main/res
+    ln -s $APP_PATH/AndroidManifest.xml app/src/main/AndroidManifest.xml
+  else
+    echo "Not a compatible project. Please make sure that you have a src folder"
+    RESULT_OF_CP=1
+  fi
+  if [ $RESULT_OF_CP == 0 ]; then EDIT_SCRIPT_FILES="true";
+  else EDIT_SCRIPT_FILES="false"; fi
+fi
+
+if [ $EDIT_SCRIPT_FILES == "true" ]; then
+  nano $APP_PATH/configure.sh
+  nano $APP_PATH/debugapp.sh
+  nano $APP_PATH/buildDebugApp.sh
+fi
